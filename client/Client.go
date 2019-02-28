40b65834aa92de224407bbe275485be5ad60b2f6
Landon Patmore <landon.patmore@gmail.com>
2019-02-28 02:17:43 -0500

    combined TCP and UDP clients into one

---
+++ client/Client.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"goNetworkTransfering/utils"
+	"net"
+	"os"
+)
+
+func main() {
+	t := determineClientType()
+	createClient(t)
+}
+
+func determineClientType() int {
+	var clientType int
+	for {
+		fmt.Println("What mode?\n1. TCP\n2. UDP")
+		_, _ = fmt.Scanf("%d", &clientType)
+
+		switch clientType {
+		case 1:
+			fallthrough
+		case 2:
+			return clientType
+		default:
+			fmt.Println("Not a client type...")
+		}
+	}
+}
+
+func createClient(t int) {
+	outputData := utils.CreateOutputData()
+
+	var serverAddress string
+	fmt.Println("Server address: ")
+	_, _ = fmt.Scanf("%s", &serverAddress)
+
+	if t == 1 {
+		outputData.Server = serverAddress
+		outputData.Type = "TCP"
+
+		conn, connError := net.Dial("tcp", serverAddress+":8721")
+
+		utils.ErrorValidation(connError)
+
+		ClientMode(conn, outputData, "TCP")
+	} else {
+		outputData.Server = serverAddress
+		outputData.Type = "UDP"
+
+		conn, connError := net.Dial("udp", serverAddress+":8274")
+
+		utils.ErrorValidation(connError)
+
+		ClientMode(conn, outputData, "UDP")
+	}
+}
+
+// Determines the mode to put the client into
+func ClientMode(conn net.Conn, outputData utils.OutputData, t string) {
+	var mode int
+
+	for {
+		fmt.Println(t)
+		fmt.Println("What mode?\n1. Measure RTT\n2. Measure Throughput (TCP Only)\n3. Measure Total Time\n4. Output\n5. Exit")
+		_, e := fmt.Scanf("%d", &mode)
+		utils.ErrorValidation(e)
+		switch mode {
+		case 1:
+			fmt.Println("Measuring Time...")
+			rtts := utils.MeasureRTT(conn)
+			outputData.RTT = append(outputData.RTT, rtts...)
+			break
+		case 2:
+			fmt.Println("Measuring Throughput...(TCP Only)")
+			throughput := utils.MeasureThroughput(conn)
+			outputData.Throughput = append(outputData.Throughput, throughput...)
+			break
+		case 3:
+			fmt.Println("Measuring Total Time...")
+			messageSizeTime := utils.MeasureTotalTime(conn)
+			outputData.MessageSizeTime = append(outputData.MessageSizeTime, messageSizeTime...)
+			break
+		case 4:
+			fmt.Println("Outputting Data...")
+			utils.CreateJSONFile(t+"_"+outputData.Server, outputData)
+			fmt.Println("Data Output...")
+		case 5:
+			fmt.Println("Exiting...")
+			utils.ErrorValidation(conn.Close())
+			os.Exit(0)
+		default:
+			fmt.Println("Error, not a selectable mode.")
+		}
+	}
+}
--- utils/Utils.go
+++ utils/Utils.go
@@ -2,18 +2,56 @@ package utils
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"time"
 )
 
 const BytesInKB = 1024
 
+type OutputData struct {
+	Client          string             `json:"client"`
+	Server          string             `json:"server"`
+	Type            string             `json:"type"`
+	RTT             [] RTT             `json:"rtt"`
+	Throughput      [] Throughput      `json:"throughput"`
+	MessageSizeTime [] MessageSizeTime `json:"message_size_time"`
+}
+
+type RTT struct {
+	MessageSize int     `json:"message_size"`
+	TotalTime   float64 `json:"total_time"`
+}
+
+type Throughput struct {
+	MessageSize int     `json:"message_size"`
+	Megabits    float64 `json:"megabits"`
+}
+
+type MessageSizeTime struct {
+	MessageSize   int     `json:"message_size"`
+	MessageAmount int     `json:"message_amount"`
+	TotalTime     float64 `json:"total_time"`
+}
+
 // Keeps track of the initial time and final time
 type TimeMeasurement struct {
 	InitialTime int64
 	FinalTime   int64
+}
+
+func CreateOutputData() OutputData {
+	return OutputData{
+		Client:          "",
+		Server:          "",
+		Type:            "",
+		RTT:             make([] RTT, 0),
+		Throughput:      make([] Throughput, 0),
+		MessageSizeTime: make([] MessageSizeTime, 0),
+	}
 }
 
 // Determines the total amount of time in milliseconds
@@ -28,7 +66,7 @@ func (r TimeMeasurement) Difference() float64 {
 
 // Gets the Round-Trip Time for data sent to a server
 func (r TimeMeasurement) GetRTT() {
-	fmt.Printf("Round Trip Time Measurement:\n\nIntitial TimeMeasurement: %d nanoseconds\nFinal TimeMeasurement: %d nanoseconds\nTotal Total TimeMeasurement: %f nanoseconds (%f milliseconds)\n", r.InitialTime, r.FinalTime, r.Difference(), r.GetTotalTimeInMilliseconds())
+	fmt.Printf("Round Trip Time Measurement:\n\nIntitial TimeMeasurement: %d nanoseconds\nFinal TimeMeasurement: %d nanoseconds\nTotal Total TimeMeasurement: %f nanoseconds (%f milliseconds)\n\n", r.InitialTime, r.FinalTime, r.Difference(), r.GetTotalTimeInMilliseconds())
 }
 
 // Creates an array filled with bytes determined by a size
@@ -74,25 +112,60 @@ func GetInBytesOrKiloBytes(bytes bool) int {
 }
 
 // Measures the Return-Trip time of a stream of bytes
-func MeasureRTT(conn net.Conn) {
-	for {
-		bytes := GetInBytesOrKiloBytes(true)
+func MeasureRTT(conn net.Conn) [] RTT {
+	var rtts [] RTT
+
+	bytes := GetInBytesOrKiloBytes(true)
+
+	for i := 0; i < 5; i++ {
+		timeMeasurement := TimeMeasurement{InitialTime: CurrentTimeNano()}
+		conn.Write(CreateFilledArray(bytes))
+
+		_, _ = bufio.NewReader(conn).ReadBytes('\n')
+		timeMeasurement.FinalTime = CurrentTimeNano()
+
+		timeMeasurement.GetRTT()
+
+		rtts = append(rtts, RTT{MessageSize: bytes, TotalTime: timeMeasurement.GetTotalTimeInMilliseconds()})
+	}
+
+	return rtts
+}
+
+// Measures the throughput in Megabits/sec
+func MeasureThroughput(conn net.Conn) [] Throughput {
+	var throughput [] Throughput
+
+	bytes := GetInBytesOrKiloBytes(false)
+
+	for i := 0; i < 5; i++ {
 
 		rtt := TimeMeasurement{InitialTime: CurrentTimeNano()}
 		conn.Write(CreateFilledArray(bytes))
 
 		_, _ = bufio.NewReader(conn).ReadBytes('\n')
+
 		rtt.FinalTime = CurrentTimeNano()
 
-		rtt.GetRTT()
+		bitsSent := float64(8 * bytes)
+		rttInNanoSeconds := rtt.Difference()
+		megabits := (bitsSent / (rttInNanoSeconds / 2)) * 1000
+		fmt.Printf("Throughput: %f Megabits/sec\n", megabits) //Converts bits/nano to megabits/sec
+
+		throughput = append(throughput, Throughput{MessageSize: bytes, Megabits: megabits})
 	}
+
+	return throughput
 }
 
 // Measures the total time it takes to send (n) amount of bytes in
 // (m) amount of messages
-func MeasureTotalTime(conn net.Conn) {
-	for {
-		bytes := GetInBytesOrKiloBytes(true)
+func MeasureTotalTime(conn net.Conn) [] MessageSizeTime {
+	var messageSizeTime [] MessageSizeTime
+
+	bytes := GetInBytesOrKiloBytes(true)
+
+	for i := 0; i < 5; i++ {
 
 		if isMultipleOfOneMegabyte(bytes) {
 			messageAmount := determineMessagesToSend(bytes)
@@ -105,11 +178,17 @@ func MeasureTotalTime(conn net.Conn) {
 
 			rtt.FinalTime = CurrentTimeNano()
 
-			fmt.Printf("Total TimeMeasurement: %f Milliseconds\n", float64(rtt.Difference())/1e6)
+			totalTime := float64(rtt.Difference()) / 1e6
+			fmt.Printf("Total TimeMeasurement: %f Milliseconds\n", totalTime)
+
+			messageSizeTime = append(messageSizeTime, MessageSizeTime{MessageSize: bytes, MessageAmount: messageAmount, TotalTime: totalTime})
 		} else {
 			fmt.Println("Not a multiple of a MegaByte.")
+			break
 		}
 	}
+
+	return messageSizeTime
 }
 
 // Sends multiple messages in sequential order
@@ -130,4 +209,9 @@ func isMultipleOfOneMegabyte(bytes int) bool {
 // amount of bytes
 func determineMessagesToSend(bytes int) int {
 	return 1048576 / bytes
+}
+
+func CreateJSONFile(name string, v interface{}) {
+	fileWriter, _ := os.Create(name + ".json")
+	ErrorValidation(json.NewEncoder(fileWriter).Encode(v))
 }
